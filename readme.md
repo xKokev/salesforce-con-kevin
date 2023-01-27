866c04d5ca46dd38bf49b5955bcf29bfffce9b73
xKokev <10kevinho@gmail.com>
2023-01-26 22:54:51 -0400

    1 coomitt

---
+++ readme.md
@@ -0,0 +1,37 @@
+  <p>
+  <img src="https://media3.giphy.com/media/H1jSPXCJmo8AZi3gdP/giphy.gif?cid=ecf05e47jj8reh8atse2iqagxfvy51z7ahrj8saecs3kgq9k&rid=giphy.gif&ct=s" width="300" height="300" align='left'/>
+  </p> 
+  
+  
+```js
+const developer = {
+  firstName: 'Kevin',
+  programmingLanguages: {
+    preference: 'JavaScript',
+    regulars: ['React', 'Next.js', 'Node', 'CSS', 'HTML'],
+  },
+  languages: ['English 🇺🇸', 'Spanish 🇪🇸', 'Italian 🇮🇹'],
+  contactMe: function () {
+    if (needHelp || wannaChat) {
+      tweetAt('@xkokev');
+    } else {
+      return false;
+    }
+  },
+  portfolio: 'https://xkokev.github.io/Kevin-Correa-portfolio/',
+};
+
+
+```
+
+<div align='center' >
+  
+![GitHub followers](https://img.shields.io/github/followers/xkokev?style=social)
+  
+</div>
+
+<p align='center'>
+<a href="https://twitter.com/xKokev"><img height="30" src="https://github.com/xKokev/xkokev/blob/main/twitter-100.png"></a>&nbsp;&nbsp;
+<a href="https://www.instagram.com/xkokev/"><img height="31" src="https://github.com/xKokev/xkokev/blob/main/igg.png"></a>&nbsp;&nbsp;
+<a href="https://www.linkedin.com/in/kevincorreasuarez/"><img height="30" src="https://github.com/xKokev/xkokev/blob/main/linkedin.png"></a>
+</p>
---
+++ site.config.ts
@@ -0,0 +1,67 @@
+import { siteConfig } from './lib/site-config'
+
+export default siteConfig({
+  // the site's root Notion page (required)
+  rootNotionPageId: 'd27f52948e7c40ff9b2c6b74b8c9589c',
+
+  // if you want to restrict pages to a single notion workspace (optional)
+  // (this should be a Notion ID; see the docs for how to extract this)
+  rootNotionSpaceId: null,
+
+  // basic site info (required)
+  name: 'Salesforce con Kevin',
+  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  author: 'Kevin',
+
+  // open graph metadata (optional)
+  description: 'Tutoriales de Salesforce, Curso de Salesforce en español',
+
+  // social usernames (optional)
+ 
+  github: 'xkokev',
+  linkedin: 'kevincorreasuarez',
+  instagram: 'xkokev',
+
+  // mastodon: '#', // optional mastodon profile URL, provides link verification
+  // newsletter: '#', // optional newsletter URL
+  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+
+  // default notion icon and cover images for site-wide consistency (optional)
+  // page-specific values will override these site-wide defaults
+  defaultPageIcon: null,
+  defaultPageCover: null,
+  defaultPageCoverPosition: 0.5,
+
+  // whether or not to enable support for LQIP preview images (optional)
+  isPreviewImageSupportEnabled: true,
+
+  // whether or not redis is enabled for caching generated preview images (optional)
+  // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
+  // environment variables. see the readme for more info
+  isRedisEnabled: false,
+
+  // map of notion page IDs to URL paths (optional)
+  // any pages defined here will override their default URL paths
+  // example:
+  //
+  // pageUrlOverrides: {
+  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
+  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
+  // }
+  pageUrlOverrides: null,
+
+  // whether to use the default notion navigation style or a custom one with links to
+  // important pages
+  //navigationStyle: 'default'
+   navigationStyle: 'custom',
+   navigationLinks: [
+     {
+       title: 'About',
+       pageId: 'f1199d37579b41cbabfc0b5174f4256a'
+     },
+     {
+      title: 'Contact',
+       pageId: '9b77c154e13042a48905913717287e49'
+     }
+   ]
+})
